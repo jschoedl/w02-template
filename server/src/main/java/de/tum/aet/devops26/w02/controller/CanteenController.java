fbd16ceca35623bee07ffcb3c26ae7b0870b3e19
Jakob Schödl <jakob.schoedl@entropy-data.com>
2026-07-23 20:20:28 +0200

    add tests & fix

--- server/src/main/java/de/tum/aet/devops26/w02/controller/CanteenController.java
+++ server/src/main/java/de/tum/aet/devops26/w02/controller/CanteenController.java
@@ -28,6 +28,8 @@ public class CanteenController {
     @GetMapping("/{canteenName}/today")
     public ResponseEntity<List<Dish>> getTodayMeals(@PathVariable String canteenName) {
         // TODO: Implement the logic to fetch today's meals for the specified canteen using the CanteenService
-        return ResponseEntity.ok(canteenService.getTodayMeals(canteenName));
+        List<Dish> meals = canteenService.getTodayMeals(canteenName);
+        if (meals.isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(meals);
     }
 }
